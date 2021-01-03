923bdc93798afc905f8ec770eda5e8791212fb10
Rakibul Islam <mri.rakib1-@gmail.com>
2021-01-03 12:31:15 +0600

    first commit

---
+++ main_package/Main.java
@@ -0,0 +1,11 @@
+package main_package;
+
+import banking.Bank;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Bank bkash = new Bank();
+        bkash.start_banking();
+    }
+}
